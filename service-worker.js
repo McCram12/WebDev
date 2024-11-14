1d119df6d34532901aed9b7c26602b45b542895d
McCram12 <148640479+McCram12@users.noreply.github.com>
2024-11-14 20:11:32 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+// service-worker.js
+const CACHE_NAME = "web-console-cache";
+const urlsToCache = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
